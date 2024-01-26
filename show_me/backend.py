d617fda5db6dc22f2143f0a0f220b9f36230b744
Jackson Burns <33505528+JacksonBurns@users.noreply.github.com>
2024-01-26 13:03:37 -0500

    year patch

--- show_me/backend.py
+++ show_me/backend.py
@@ -237,10 +237,8 @@ class G:
         # we could make this function async and display stuff real-time
         json_set = []
         j = None
-        if start_year >= 2020:
-            raise RuntimeError("The start year should be smaller than 2020.")
         # FIXME: default to current date+time
-        years_to_scan = iter(range(start_year, 2020))
+        years_to_scan = iter(range(start_year, 2024))
         year = next(years_to_scan)
         while True:
             query = self._get_template_query(year, last_response=j)
